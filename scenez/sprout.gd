93de6985be5ff958c80783589370c57e90b78be6
myrrhpansies <irisjh@protonmail.com>
2023-04-27 15:15:34 -0700

    basic movement

--- scenez/sprout.gd
+++ scenez/sprout.gd
@@ -1,13 +1,19 @@
 extends CharacterBody2D
 
 @export var speed : int = 35
+@onready var animations = $AnimationPlayer
 
 func handleInput():
 	var moveDirection = Input.get_vector("left", "right", "up", "down")
 	velocity = moveDirection * speed
+	
+func updateAnimation():
+	if Input.is_action_just_pressed("left"):
+		animations.play("left")
 	if Input.is_action_just_pressed("right"):
-		$sproutSprite.flip_h()
+		animations.play("right")	
 
 func _physics_process(delta):
+	updateAnimation()
 	handleInput()
 	move_and_slide()
